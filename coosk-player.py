7c70967aa13db2483f79d6562eef7d6046e77c0b
COOSK-KUNKUN <532866709@qq.com>
2017-09-27 23:43:15 +0800

    1

--- coosk-player.py
+++ coosk-player.py
@@ -1,6 +1,11 @@
 # -*- coding: UTF-8 -*-
 
 import wx
+import os
+import wx.lib.buttons as buttons
+
+dirName = os.path.dirname(os.path.abspath(__file__))
+bitmapCoosk = os.path.join(dirName, 'bitmaps')
 
 class mycoosk(wx.Frame):
     """docstring for mycoosk"""
@@ -8,51 +13,67 @@ class mycoosk(wx.Frame):
         super(mycoosk, self).__init__(parent, title = title , size = (320,173) )
         panel = wx.Panel(self)
 
-        image = wx.Image("1.bmp",wx.BITMAP_TYPE_BMP).ConvertToBitmap()
+        image_picture = wx.Image("1.bmp",wx.BITMAP_TYPE_BMP).ConvertToBitmap()
         # wx.Image(name, type=wx.BITMAP_TYPE_ANY, index=-1)
         # 从一个文件载入一个图像
 
-
-        self.button = wx.BitmapButton(panel, -1,image, pos = (10,10),size=(70,70))
+        self.button = wx.BitmapButton(panel, -1,image_picture, pos = (10,10),size=(70,64))
         self.Bind(wx.EVT_BUTTON,self.image,self.button)
         self.button.SetDefault()
         # 载入图片的窗口
         
-        self.CreateStatusBar()
+        image_picture = wx.Image("1.bmp",wx.BITMAP_TYPE_BMP).ConvertToBitmap()
 
+        
+        
+        #***************************************************************************
+               
         self.filemenu = wx.Menu()
         #设置菜单
 
-        self.filemenu.Append(wx.ID_ABOUT, u"About", u"about this program")
+        self.CreateStatusBar()
+
+        self.filemenu.Append(wx.ID_ABOUT, u"打开", u"Open the music")
         self.filemenu.AppendSeparator()
-        self.filemenu.Append(wx.ID_ABOUT, u"exit", u"终止应用程序")
+        self.filemenu.Append(wx.ID_EXIT, u"退出", u"Exit the software")
         #wx.ID_ABOUT和wx.ID_EXIT是wxWidgets提供的标准ID
 
         self.menuBar = wx.MenuBar()
-        self.menuBar.Append(self.filemenu, u"flie")
+        self.menuBar.Append(self.filemenu, u"文件")
         self.SetMenuBar(self.menuBar)
         self.Show(True)
         #创建菜单栏
                
          
 
-    #***************************************************************************************#    
-          
+        #***************************************************************************************#    
+    
+
+
+        image_play = wx.Image("play.bmp",wx.BITMAP_TYPE_BMP).ConvertToBitmap()
+
+
+        self.button = wx.BitmapButton(panel, -1,image_play, pos = (90,10),size=(28.6,28.6))
+        self.Bind(wx.EVT_BUTTON,self.image_play,self.button)
+        self.button.SetDefault()
+        
+        image_play = wx.Image("play.bmp",wx.BITMAP_TYPE_BMP).ConvertToBitmap()
+
 
         
 
-    #***************************************************************************************#    
+        #***************************************************************************************#    
         
 
 
 
     def image(self,event):
-        print self.button.GetStringSelection(),' is clicked from Radio Box' 
+        pass
 
-    
+    def image_play(self,event):
+        pass   
 
 app = wx.App()
-
 
 frame = mycoosk(None, 'COOSK')
 
