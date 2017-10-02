3e47c62643ad5235dfad9b24e5981042ca5b97cb
COOSK-KUNKUN <532866709@qq.com>
2017-10-02 23:40:51 +0800

    1

--- coosk-player.py
+++ coosk-player.py
@@ -3,6 +3,9 @@
 import wx
 import os
 import wx.lib.buttons as buttons
+from win32com.client import Dispatch
+
+
 
 dirName = os.path.dirname(os.path.abspath(__file__))
 bitmapCoosk = os.path.join(dirName, 'bitmaps')
@@ -16,6 +19,7 @@ class mycoosk(wx.Frame):
         image_picture = wx.Image("1.bmp",wx.BITMAP_TYPE_BMP).ConvertToBitmap()
         # wx.Image(name, type=wx.BITMAP_TYPE_ANY, index=-1)
         # 从一个文件载入一个图像
+        # 左侧头像
 
         self.button = wx.BitmapButton(panel, -1,image_picture, pos = (10,10),size=(70,64))
         self.Bind(wx.EVT_BUTTON,self.image,self.button)
@@ -26,16 +30,16 @@ class mycoosk(wx.Frame):
 
         
         
-        #***************************************************************************
+        ###########################################################################################
                
         self.filemenu = wx.Menu()
         #设置菜单
 
         self.CreateStatusBar()
 
-        self.filemenu.Append(wx.ID_ABOUT, u"打开", u"Open the music")
+        self.open = self.filemenu.Append(wx.ID_ABOUT, u"打开", u"Open the music")
         self.filemenu.AppendSeparator()
-        self.filemenu.Append(wx.ID_EXIT, u"退出", u"Exit the software")
+        self.exit = self.filemenu.Append(wx.ID_EXIT, u"退出", u"Exit the software")
         #wx.ID_ABOUT和wx.ID_EXIT是wxWidgets提供的标准ID
 
         self.menuBar = wx.MenuBar()
@@ -46,33 +50,83 @@ class mycoosk(wx.Frame):
                
          
 
-        #***************************************************************************************#    
+        ###########################################################################################
     
-
+        # 各种图标
 
         image_play = wx.Image("play.bmp",wx.BITMAP_TYPE_BMP).ConvertToBitmap()
+        # 开始播放音乐键图标
 
-
-        self.button = wx.BitmapButton(panel, -1,image_play, pos = (90,10),size=(28.6,28.6))
+        self.button = wx.BitmapButton(panel, -1,image_play, pos = (90,10),size=(31,31))
         self.Bind(wx.EVT_BUTTON,self.image_play,self.button)
         self.button.SetDefault()
         
         image_play = wx.Image("play.bmp",wx.BITMAP_TYPE_BMP).ConvertToBitmap()
 
 
+        image_stop = wx.Image("stop.bmp",wx.BITMAP_TYPE_BMP).ConvertToBitmap()
+        # 暂停音乐键图标
+
+        self.button = wx.BitmapButton(panel, -1,image_stop, pos = (126,10),size=(31,31))
+        self.Bind(wx.EVT_BUTTON,self.image_stop,self.button)
+        self.button.SetDefault()
         
+        image_play = wx.Image("stop.bmp",wx.BITMAP_TYPE_BMP).ConvertToBitmap()
 
-        #***************************************************************************************#    
+
+        image_next = wx.Image("next.bmp",wx.BITMAP_TYPE_BMP).ConvertToBitmap()
+        # 下一首音乐键图标
+
+        self.button = wx.BitmapButton(panel, -1,image_next, pos = (162,10),size=(31,31))
+        self.Bind(wx.EVT_BUTTON,self.image_next,self.button)
+        self.button.SetDefault()
         
+        image_next = wx.Image("next.bmp",wx.BITMAP_TYPE_BMP).ConvertToBitmap()
 
 
+
+
+        ###########################################################################################
+        
+        # 各种事件
+
+        self.Bind(wx.EVT_MENU,self.OnOpen,self.open)
+        self.Bind(wx.EVT_MENU,self.Exit,self.exit)
+        self.wmp=Dispatch('WMPlayer.OCX')
 
     def image(self,event):
         pass
 
     def image_play(self,event):
+        self.wmp.controls.play()
         pass   
 
+    def image_stop(self,event):
+        self.wmp.controls.stop()
+        pass
+
+    def image_next(self,event):
+        self.wmp.controls.next()
+        pass
+
+    def OnOpen(self,event):
+        FileDialog = wx.FileDialog(None, 'open', wildcard='*.wav|*.mp3')
+        if FileDialog.ShowModal() == wx.ID_OK:
+        # ShowModal() 显示对话框.
+        # 如果用户单击OK按钮返回wx.ID_OK，否则 wx.ID_CANCEL
+            
+            self.filename=FileDialog.GetPath()
+            if self.filename:
+                media=self.wmp.newMedia(self.filename)
+                self.wmp.currentPlaylist.appendItem(media)
+            FileDialog.Destroy()
+
+        pass
+
+    def Exit(self,event):
+        app.ExitMainLoop()
+        # ExitMainLoop() 快速关闭程序
+        pass
 app = wx.App()
 
 frame = mycoosk(None, 'COOSK')
